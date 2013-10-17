2eabe4f6c967e788018cdc4ebae4091ad5156fa7
Jody Fanning <jody.fanning@basware.com>
2013-10-17 11:55:22 +0300

    Remove jasmine-jquery

--- src/main/resources/jasmineEnv.js
+++ src/main/resources/jasmineEnv.js
@@ -4,5 +4,4 @@
 EnvJasmine.loadLibGlobal("rjs/r-2.1.9.js");
 EnvJasmine.loadLibGlobal("envjs/env.rhino.1.2.js");
 EnvJasmine.loadLibGlobal("jasmine/jasmine.js");
-EnvJasmine.loadLibGlobal("jasmine-jquery/jasmine-jquery-1.5.91.js");
 EnvJasmine.loadLibGlobal("jasmine-rhino-reporter/jasmine-rhino-reporter.js");
